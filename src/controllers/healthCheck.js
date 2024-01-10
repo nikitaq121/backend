5659612a91da2a57c7ca9aa4539609b7c8f557a9
nikitaq121 <nikitaq121@gmail.com>
2024-01-10 19:03:14 +0100

    fixed healthCheck route

--- src/controllers/healthCheck.js
+++ src/controllers/healthCheck.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/healthcheck', (req, res) => {
+routes.get('/', (req, res) => {
   const date = new Date().toISOString();
   res.status(200).json({ server_status: 'Server is healthy', check_time: date });
 });
